c836774c5ce73640575ace1040c947c2fc20c45e
MdRafsanZaman <mrz281@nyu.edu>
2026-01-31 00:03:26 -0500

    Document handler completed for now, gitignore improved

--- DocHandler.py
+++ DocHandler.py
@@ -1,25 +1,37 @@
 import os.path
+import os
 from datetime import datetime, timezone, timedelta
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from dotenv import load_dotenv
+from pathlib import Path
 
 class DocHandler:
 
     def __init__(self):
+        load_dotenv()
         self.creds = None
         self.validate()
 
 
     def validate(self):
+        self.SCOPES = [os.getenv('DRIVE_SCOPE')]
+        authen_path = str(Path(__file__).parent) + os.getenv('AUTHENTIFICATION_PATH')
+        token_path = str(Path(__file__).parent) + os.getenv('TOKEN_PATH')
+        if os.path.exists(token_path):
+            self.creds = Credentials.from_authorized_user_file(token_path, self.SCOPES)
         if not self.creds or not self.creds.valid:
             if self.creds and self.creds.expired and self.creds.refresh_token:
                 self.creds.refresh(Request())
             else:
-                flow = InstalledAppFlow.from_client_secrets_file()
-                creds = flow.run_local_server(port=0)
+                flow = InstalledAppFlow.from_client_secrets_file(authen_path, self.SCOPES)
+                self.creds = flow.run_local_server(port=0)
+            with open(token_path, 'w') as token:
+                token.write(self.creds.to_json())
+
 
 
     def last_modified(self):
@@ -27,5 +39,7 @@ class DocHandler:
 
     def pull_doc(self):
         pass
+
+test = DocHandler()
 
     
