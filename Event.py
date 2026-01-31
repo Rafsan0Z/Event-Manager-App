bae4c162b9b18d2409b30d493134c3ff5eb332dd
MdRafsanZaman <mrz281@nyu.edu>
2026-01-30 21:37:19 -0500

    Switching from java to python, Event and Main classes added

---
+++ Event.py
@@ -0,0 +1,32 @@
+class Event:
+    
+    def __init__(self, name, date, duration = None, notes = None):
+        self.name = name
+        self.date = date
+        if duration: self.duration = duration
+        else: self.duration = 0
+        if notes: self.notes = notes
+        else: self.notes = ''
+
+        self.is_done = False
+        self.synchronous = False
+        self.notes_taken = False
+        self.recorded = False
+    
+    def edit_name(self):
+        pass
+
+    def edit_duration(self):
+        pass
+
+    def add_notes(self):
+        pass
+
+    def delete_notes(self):
+        self.notes = ''
+
+    def edit_time(self):
+        pass
+
+    def edit_date(self):
+        pass
---
+++ Main.py
@@ -0,0 +1,68 @@
+class Main:
+
+    def __init__(self):
+        pass
+
+    def grab_events():
+        pass
+
+    def flush_events():
+        pass
+
+    def process_duration():
+        pass
+
+    def upcoming_event():
+        pass
+
+    def recent_event():
+        pass
+
+    def get_events_by_date():
+        pass
+
+    def todays_events():
+        pass
+
+    def tomorrows_events():
+        pass
+
+    def yesterdays_events():
+        pass
+
+    
+    def add_event():
+        pass
+
+    def remove_event():
+        pass
+
+    def edit_notes():
+        pass
+
+    def delete_notes():
+        pass
+
+    def find_max_events_month():
+        pass
+
+    def find_max_events_day():
+        pass
+
+    
+    
+    def plot_events():
+        pass
+    
+    def plot_events_daily():
+        pass
+
+
+    def total_events():
+        pass
+
+    def total_time():
+        pass
+
+
+
