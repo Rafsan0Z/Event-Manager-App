cb0fc111ed1ef94c3caa94cd43e4d203468e1240
MdRafsanZaman <mrz281@nyu.edu>
2026-02-01 18:05:28 -0500

    special limitations for Year added and tested, might update later

--- Year.py
+++ Year.py
@@ -4,8 +4,29 @@ class Year(MonthList):
 
     def __init__(self, number):
         self.number = number
+        self.__claimed_months = []
         super().__init__()
     
+    def __setitem__(self, i, month):
+        self.check_month_item(month)
+        current_month_name = self[i].month
+
+        if month.month in self.__claimed_months:
+            if current_month_name != month.month:
+                raise Exception("ERROR")
+        else:
+            self.__claimed_months.remove(current_month_name)
+            self.__claimed_months.append(month.month)
+
+        self.months[i] = month
+
+    def insert(self, i, month):
+        self.check_month_item(month)
+        if month.month in self.__claimed_months:
+            raise Exception("This month is already claimed!")
+        else:
+            self.__claimed_months.append(month.month)
+            self.months.insert(i, month)
 
     def give_events(self):
         for month in self.months:
@@ -13,7 +34,19 @@ class Year(MonthList):
                 for event in day:
                     yield event
 
+    def __str__(self): #will be changed
+        result = ''
+        for month in self.months:
+            result += month.month + '\n'
+        return result
+
+
 
 # test = Year(2026)
 # test.append(Month('january'))
+# test.append(Month('february'))
+# test.append(Month('march'))
+# test[-1] = Month('april')
+# test.append(Month('march'))
+# print(test)
 # print(test[0])
