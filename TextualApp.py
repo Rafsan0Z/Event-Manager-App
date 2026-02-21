dd390ae67a9d9705fd2501e0920f012937b4b48a
MdRafsanZaman <mrz281@nyu.edu>
2026-02-21 14:25:02 -0500

    Textual CSS added and buttons are now centered

--- TextualApp.py
+++ TextualApp.py
@@ -1,24 +1,38 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header
+from textual.widgets import Footer, Header, Button, Digits
+from textual.containers import HorizontalGroup, VerticalScroll, VerticalGroup, Center
+from textual import on
+
+class HorizTest(VerticalGroup):
+
+    def compose(self) -> ComposeResult:
+        yield Button("View Events", id="view_events")
+        yield Button("Add Events", id="add_events")
+        yield Button("Edit Events", id="edit_events")
+        yield Button("Plot Events", id="plot_events")
+        yield Button("Info", id="info")
+        yield Button("Exit", id="exit-btn", variant="error")
 
 class TestApp(App):
 
+    CSS_PATH = "test.tcss"
     LIGHT_MODE = 'textual-light'
     DARK_MODE = 'textual-dark'
-    BINDINGS = [('d', 'dark_toggle', 'Toggle Dark Mode!'),
-                ('q', 'exit', "Exit App")
+    BINDINGS = [('d', 'dark_toggle', 'Toggle Dark Mode!')
     ]
+
+    @on(Button.Pressed, "#exit-btn")
+    def exit_app(self):
+        self.exit()
 
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
+        yield VerticalScroll(HorizTest(), classes="buttons")
 
     def action_dark_toggle(self) -> None:
         self.theme = (
             self.DARK_MODE if self.theme == 'textual-light' else self.LIGHT_MODE
         )
-
-    # def exit(self) -> None:
-    #     self.exit()
     
 TestApp().run()
